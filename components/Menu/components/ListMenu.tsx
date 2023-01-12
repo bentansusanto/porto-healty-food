fdba7315766a6f9fe564f96011c8652ec16e01c8
bentansusanto <tansusanto194@gmail.com>
2023-01-12 22:20:31 +0700

    update : filter menu by category

--- components/Menu/components/ListMenu.tsx
+++ components/Menu/components/ListMenu.tsx
@@ -4,36 +4,37 @@ import Star from "../../../public/assets/Star.svg";
 import {IoMdAdd} from 'react-icons/io'
 
 interface listMenuProps {
-  menu: listMenuContent[];
   mobile: boolean;
+  menu : listMenuContent[];
 }
 
 interface listMenuContent {
   title: string;
+  category :string;
   price: string;
   rating: string;
   image: string;
   sort_desc: string;
 }
 
-const listMenu: React.FC<listMenuProps> = (props) => {
+const listMenu: React.FC<listMenuProps> = ({mobile, menu}) => {
   return (
     <div
       className={`${
-        props.mobile ? "grid-cols-2 gap-x-5 mx-6" : "grid-cols-4 gap-x-10 mx-20"
+        mobile ? "grid-cols-2 gap-x-5 mx-6" : "grid-cols-4 gap-x-10 mx-20"
       } grid  mt-5 gap-y-10 z-0 relative`}
     >
-      {props.menu.map((val, idx) => (
+      {menu.map((val, idx) => (
         <div key={idx} className="space-y-2">
           <div className="relative">
             <Image src={val.image} alt="" width={350} height={350} />
             <div
               className={`${
-                props.mobile ? "space-x-1 py-2.5" : "space-x-2  py-3"
+                mobile ? "space-x-1 py-2.5" : "space-x-2  py-3"
               } absolute top-0 px-2.5 right-0 bg-[#F8F8F8] flex items-center ml-auto rounded-tl-lg rounded-br-lg shadow-md`}
             >
               <Image src={Star} alt="" className="w-4" />
-              <p className={`${props.mobile ? "text-[.8rem]" : "text-[1rem]"}`}>
+              <p className={`${mobile ? "text-[.8rem]" : "text-[1rem]"}`}>
                 {val.rating}
               </p>
             </div>
@@ -41,24 +42,24 @@ const listMenu: React.FC<listMenuProps> = (props) => {
           <div className="space-y-2">
             <h4
               className={`${
-                props.mobile ? "text-[.9rem]" : "text-[1rem]"
+                mobile ? "text-[.9rem]" : "text-[1rem]"
               } font-semibold`}
             >
               {val.title}
             </h4>
             <p
               className={`${
-                props.mobile ? "text-[.8rem]" : "text-[.9rem]"
+                mobile ? "text-[.8rem]" : "text-[.9rem]"
               } text-gray`}
             >
               {val.sort_desc}
             </p>
             <div className="flex items-center">
-                <button className={`${props.mobile ? "px-2.5" : "px-3"} flex bg-hijau text-white items-center py-2 rounded-sm`}>
+                <button className={`${mobile ? "px-2.5" : "px-3"} flex bg-hijau text-white items-center py-2 rounded-sm`}>
                     <IoMdAdd/>
                     <p>Add</p>
                 </button>
-                <p className={`${props.mobile ? "text-[.9rem]" : "text-[1.1rem]"} font-semibold ml-auto`}>$ {val.price}</p>
+                <p className={`${mobile ? "text-[.9rem]" : "text-[1.1rem]"} font-semibold ml-auto`}>$ {val.price}</p>
             </div>
           </div>
         </div>
--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -6,28 +6,69 @@ import menuIcon from "../../public/assets/menu.png";
 import ListMenu from "./components/ListMenu";
 import styles from "./Menu.module.css";
 
+
+interface listMenu{
+  title: string;
+  category : string;
+  price: string;
+  rating: string;
+  image: string;
+  sort_desc: string;
+}
+
+
 const Menu = () => {
   const [Mobile, setMobile] = useState(false);
-  const [dataMenu, setDataMenu] = useState([]);
+  const [dataMenus, setDataMenu] = useState<listMenu[]>([]);
   const baseURL = "http://localhost:5000";
   const [open, setOpen] = useState(false);
   const [openCategory, setOpenCategory] = useState(false);
+  const [category, setCategory] = useState<string>("")
+  const [price, setPrice] = useState<number | string>("")
+  const [rating, setRating] = useState< number |string>("")
+  const [filterMenu, setFilterMenu] = useState<listMenu[]>([])
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
+  // fetch menu
   const fetchMenu = async () => {
     await axios
       .get(`${baseURL}/menu`)
       .then((response) => {
-        console.log(response.data);
+        // console.log(response.data);
         setDataMenu(response.data);
       })
       .catch((err) => {
         console.log(err);
       });
   };
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  // filter menu by category
+  useEffect(() => {
+    if(category == ""){
+      setFilterMenu(dataMenus);
+    }else{
+      setFilterMenu(dataMenus.filter((data) => data.category === category))
+    }
+  }, [dataMenus, category])
+
+  //filter by high price and low price
+  useEffect(() => {
+    if(price === "" && rating === ""){
+      setFilterMenu(dataMenus);
+    }else if(price === 'low'){
+        setFilterMenu(dataMenus.filter((data) => data.price <= price));
+    }else if(rating === 'low'){
+      setFilterMenu(dataMenus.filter((data) => data.rating < rating))
+    }
+  },[price, rating, dataMenus]) 
+
+
+  const handleClick = (e : React.MouseEvent<HTMLButtonElement>) => {
+      setCategory(e.currentTarget.value);
+  }
+
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -68,17 +109,22 @@ const Menu = () => {
             {/* Category */}
             <div className="relative">
               <div
-                onClick={() => setOpenCategory(!openCategory)}
                 className="flex items-center space-x-3 py-4"
               >
                 <Image src={menuIcon} alt="" className="w-[2rem]" />
-                <p>All Menu</p>
-                <BsChevronDown />
+                <p>
+                  <button value={""} onClick={handleClick}>All Menu</button>
+                </p>
+                <BsChevronDown onClick={() => setOpenCategory(!openCategory)}/>
               </div>
               {openCategory && (
                 <ul className="bg-[#dadada] absolute z-20 mt-2 py-2.5 px-3 space-y-3 w-full rounded-sm">
-                  <li>Healty Food</li>
-                  <li>Fast Food</li>
+                  <li>
+                    <button value={'Healty Food'} onClick={handleClick}>Healty Food</button>
+                  </li>
+                  <li>
+                    <button value={'Fast Food'} onClick={handleClick}>Fast Food</button>
+                  </li>
                 </ul>
               )}
             </div>
@@ -88,13 +134,23 @@ const Menu = () => {
                 onClick={() => setOpen(!open)}
                 className="flex items-center space-x-3 bg-[#DADADA] py-2 px-2.5 rounded-sm"
               >
-                <p>Most Recent</p>
-                <BsChevronDown />
+                <p>Most Recent</p> 
+                <BsChevronDown /> 
               </button>
               {open && (
                 <ul className="bg-[#dadada] absolute z-20 mt-5 py-2.5 px-3 space-y-3 w-full">
-                  <li>Low Price</li>
-                  <li>High Price</li>
+                  <li>
+                    <button onClick={() => setPrice('low')}>Low Price</button>
+                    </li>
+                  <li>
+                    <button>High Price</button>
+                    </li>
+                  <li>
+                    <button>Low Rating</button>
+                    </li>
+                  <li>
+                    <button>High Rating</button>
+                    </li>
                 </ul>
               )}
             </div>
@@ -105,9 +161,15 @@ const Menu = () => {
             <div className="flex items-center">
               <Image src={menuIcon} alt="" className="w-9" />
               <div className="flex space-x-10 ml-4">
-                <p>All Menu</p>
-                <p>Healty Food</p>
-                <p>Fast Food</p>
+                <p>
+                  <button onClick={handleClick} value={""}>All Menu</button>
+                  </p>
+                <p>
+                  <button onClick={handleClick} value={'Healty Food'}>Healty Food</button>
+                  </p>
+                <p>
+                  <button onClick={handleClick} value={'Fast Food'}>Fast Food</button>
+                  </p>
               </div>
             </div>
             {/* Most Recent */}
@@ -125,15 +187,25 @@ const Menu = () => {
                 <ul
                   className={`bg-[#dadada] py-2.5 px-3 z-20 absolute mt-2 w-full rounded-sm space-y-3 cursor-pointer`}
                 >
-                  <li>Low Price</li>
-                  <li>High Price</li>
+                 <li>
+                    <button>Low Price</button>
+                    </li>
+                  <li>
+                    <button>High Price</button>
+                    </li>
+                  <li>
+                    <button>Low Rating</button>
+                    </li>
+                  <li>
+                    <button>High Rating</button>
+                    </li>
                 </ul>
               )}
             </div>
           </div>
         )}
       </div>
-      <ListMenu menu={dataMenu} mobile={Mobile} />
+      <ListMenu menu={filterMenu} mobile={Mobile} />
     </div>
   );
 };
